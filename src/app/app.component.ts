482ed94b83d3dbaa92dfd853119843464d514e1d
Alex Rote <level23archbard@gmail.com>
2021-08-02 00:23:29 -0500

    Starting project with basic board and card listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lxs-cards',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showSettingsTrigger', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('150ms ease-in', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('150ms ease-in', style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'cards';
+
+  showSettings = false;
+
+  onClickSettings(): void {
+    this.showSettings = true;
+  }
+
+  onSettingsClose(): void {
+    this.showSettings = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StorageModule } from '@level23archbard/storage-service';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { BoardComponent } from './board/board.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CardComponent,
+    SettingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    StorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
